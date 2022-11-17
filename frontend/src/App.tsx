d91e2069ad404b0289171a55c75a54bdf59ca337
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-17 19:13:57 -0300

    feat: Iniciando página Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />} 
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
