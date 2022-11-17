b0960ac46ed906163642c8972c4f87dab5a9e7df
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-17 15:23:11 -0300

    feat: Instalação e uso do pacote cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import 'express-async-errors';
+import * as cors from 'cors';
 import * as express from 'express';
 import routes from './routes';
 import errorHandler from './middlewares/errors.middleware';
@@ -17,13 +18,14 @@ export default class App {
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Methods', 'GET,POST');
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
   this.app.use(express.json());
   this.app.use(accessControl);
+  this.app.use(cors());
   this.app.use('/users', routes.userRouter);
   this.app.use('/accounts', routes.accountRouter);
   this.app.use('/transactions', routes.transactionRouter);
