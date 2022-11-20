ee908bf1dea646226160b73fe0b52610a9f7036b
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-20 13:07:21 -0300

    feat: Removendo LoginForm e atualizando para receber estilo

--- frontend/src/components/Form/Button.tsx
+++ frontend/src/components/Form/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonStyle: 'primary' | 'secondary' | 'none';
 }
 
-export default function Button({buttonStyle, ...props}: ButtonProps) {
+export default function Button({buttonStyle, className, ...props}: ButtonProps) {
   const [buttonStyles] = useState({
     primary: 'bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-violet-600',
     secondary: 'bg-zinc-900 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-zinc-800',
@@ -14,7 +14,7 @@ export default function Button({buttonStyle, ...props}: ButtonProps) {
   return (
     <button
       {...props}
-      className={buttonStyles[buttonStyle]}
+      className={buttonStyles[buttonStyle] + className}
     />
   )
 }
