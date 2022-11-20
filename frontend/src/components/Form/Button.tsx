e43994a216424def43955a7d0ebb2b2876edd192
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-20 15:19:41 -0300

    fix: Corriginfo useState para usar o hover do estilo

--- frontend/src/components/Form/Button.tsx
+++ frontend/src/components/Form/Button.tsx
@@ -6,8 +6,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button({buttonStyle, className, ...props}: ButtonProps) {
   const [buttonStyles] = useState({
-    primary: 'bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-violet-600',
-    secondary: 'bg-zinc-900 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-zinc-800',
+    primary: 'bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-violet-600 ',
+    secondary: 'bg-zinc-900 px-5 h-12 rounded-md font-semibold flex items-center justify-center gap-3 hover:bg-zinc-800 ',
     none: ''
   });
 
