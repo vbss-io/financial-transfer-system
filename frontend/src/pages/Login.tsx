cba1a49dc57d5e56f2125aded91764ebbabc3a5e
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-20 10:58:07 -0300

    feat: Página de login

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import LoginForm from "../components/Form/LoginForm"
+
+export default function Login() {
+  return(
+    <div className="
+      w-screen
+      h-screen
+      flex
+      flex-row
+      items-center
+      justify-center
+      bg-white-smoked">
+      <LoginForm />
+    </div>
+  )
+}
