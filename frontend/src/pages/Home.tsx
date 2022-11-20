73527feda3fddfab8d2b4ddabc2e43ea6ba0ad34
vitorbss12 <vitor_bss10@hotmail.com>
2022-11-20 10:59:03 -0300

    feat: Iniciando a página Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,40 @@
-function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Home
-    </h1>
-  );
+import { useEffect, useState } from 'react';
+import * as requests from '../utils/requests';
+
+interface Balance {
+  balance: number;
 }
 
-export default Home;
+interface User {
+  id: number;
+  username: string;
+  token: string;
+}
+
+export default function Home() {
+  const [balance, setBalance] = useState({} as Balance);
+  const [user, setUser] = useState({} as User);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user') as any);
+    setUser(user);
+    requests.setToken(user.token);
+
+    const getBalance = async () => {
+      const response = await requests.getBalance();
+      setBalance(response);
+    };
+    getBalance();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p>
+        {user.username}
+      </p>
+      <p>
+        {balance.balance}
+      </p>
+    </div>
+  );
+}
